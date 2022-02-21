d290aafaa43ba8d6d90a0948a5e573e91b74d3a4
piggychaa <98593460+piggychaa@users.noreply.github.com>
2022-02-21 12:45:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL1}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Tanya ke https://t.me/SharingUserbot untuk Bantuan"
+                    "\nBot Berhenti. Tanya ke https://t.me/UnrealPiggy untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL2:
@@ -100,7 +100,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/UnrealPiggy untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -114,13 +114,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/UnrealPiggy untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/TeleUdahRusak"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/UnrealPiggy"
         )
         self.username = usr_bot_me.username
 
