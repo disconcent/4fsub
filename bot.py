9a4da150bb9734ca443c88b90ac9d5f4d9564b5b
supermanbot12 <98998991+supermanbot12@users.noreply.github.com>
2022-08-02 12:08:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL1}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Tanya ke https://t.me/UnrealPiggy untuk Bantuan"
+                    "\nBot Berhenti. Tanya ke https://t.me/gkushskap untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL2:
@@ -114,13 +114,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/UnrealPiggy untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/exotisidbos untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/UnrealPiggy"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/gkushskap"
         )
         self.username = usr_bot_me.username
 
