a63f02e27f1b89c9e90fdd976e47ee1f130b2a22
supermanbot12 <98998991+supermanbot12@users.noreply.github.com>
2022-08-02 12:09:04 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/UnrealPiggy untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/exotisidbos untuk Bantuan"
                 )
                 sys.exit()
         try:
